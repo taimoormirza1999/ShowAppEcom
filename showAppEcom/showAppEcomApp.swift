3decf2ee3680630c5b91b4da6b1833b222aa5591
Taimoor Hamza <taimoorhamza1999@gmail.com>
2025-06-18 11:43:56 +0400

    Initial Commit

---
+++ showAppEcom/showAppEcomApp.swift
@@ -0,0 +1,17 @@
+//
+//  showAppEcomApp.swift
+//  showAppEcom
+//
+//  Created by Taimoor Hamza on 18/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct showAppEcomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
